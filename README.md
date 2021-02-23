6d040f7e10a9ed70d6740fdd08902be398686649
Brandon Foltz <bsfoltzguitar@gmail.com>
2021-02-22 19:20:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bitflip Scanner
+
+Have you ever wondered how often your memory is silently corrupted by cosmic rays, terrestrial radiation, or boring old heat? Now you can find out!
+
+This program allocates a large chunk of memory and populates it with predictable values according to some function of the array index. Then the program scans the memory at regular intervals to ensure that the value at the expected location is still the value we expect it to be. If it's not, we have a bitflip event!
+
+In order for this to work reliably, you should run it on a mostly-idle machine with enough free RAM to ensure that this program will not be swapped out to disk. If the pages of memory get swapped, they're re-written to memory when we need to scan again thus erasing any evidence of a bitflip if one occurred. 
+
+You can compile and run the program with Cargo like so:
+
+```bash
+cargo run --release -- -s 2 >> bitflip_log.txt
+```
+
+The parameter -s for "scale" corresponds to how many Gibibytes of memory we allocated for scanning. I chose the word "scale" because the amount of memory allocated directly determines the physical size of our "detector". A larger detector will detect events more often than a smaller one. All that said, a given amount of memory on one machine may not be physically the same size as that on another machine, so "scale" represents a relationship of proportions and not exact measurements. 
---
+++ src/main.rs
@@ -0,0 +1,68 @@
+#[macro_use]
+extern crate clap;
+
+use std::vec::*;
+use std::convert::TryInto;
+use std::{thread, time};
+use clap::App;
+
+fn main() {
+    let matches = App::new("Bitflip Scanner")
+        .args_from_usage("-s [scale] 'Number of Gibibytes to allocate for bitflip detection. Defaults to 1.'")
+        .get_matches();
+
+    let scale: usize = value_t!(matches, "scale", usize).unwrap_or(1);
+    let num_bytes: usize = scale*1024*1024*1024;
+    let mut memory: Vec<u8> = Vec::with_capacity(num_bytes);
+
+    // Populate the memory in a predictable way
+    println!("Populating memory with {} bytes...", num_bytes);
+    for i in 0..num_bytes {
+        memory.push(get_value_at_index(i))
+    }
+
+    println!("Scanning for bitflips...");
+    loop {
+        thread::sleep(time::Duration::from_millis(60000));
+        let count = check_memory(&mut memory);
+        if count > 0 {
+            println!("Found {} bit flips.", count); // TODO include a timestamp with this output
+        }
+    }
+}
+
+fn get_value_at_index(index: usize) -> u8 {
+    (index % 256).try_into().unwrap()
+}
+
+fn check_memory(mem: &mut Vec<u8>) -> u64 {
+    let mut count: u64 = 0;
+    for i in 0..mem.capacity() {
+        if mem[i] != get_value_at_index(i) {
+            count = count + 1;
+            mem[i] = get_value_at_index(i);
+        }
+    }
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_value_at_index() {
+        assert_eq!(0, get_value_at_index(0));
+        assert_eq!(0, get_value_at_index(256));
+        assert_eq!(1, get_value_at_index(257));
+    }
+
+    #[test]
+    fn test_check_memory_finds_errors() {
+        let mut mem: Vec<u8> = vec![0,1,4];
+        assert_eq!(1, check_memory(&mut mem));
+
+        // The error is fixed in the previous pass
+        assert_eq!(0, check_memory(&mut mem));
+    }
+}
